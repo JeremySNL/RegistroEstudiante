2d9ffdd902567ad31aa6fad73f7cd2a23270308c
Jeremy Sanchez Cabrera <jeremy_sanchez@ucne.edu.do>
2026-01-21 11:28:10 -0400

    Agrega DbSet de Asignaturas a Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -7,4 +7,6 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     public DbSet<Estudiantes> Estudiantes { get; set; }
+
+    public DbSet<Asignaturas> Asignaturas { get; set; }
 }
