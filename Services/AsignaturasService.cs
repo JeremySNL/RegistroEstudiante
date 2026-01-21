422c9ca746340e9ec49054cc7c7042bd13659e62
Jeremy Sanchez Cabrera <jeremy_sanchez@ucne.edu.do>
2026-01-21 11:58:21 -0400

    Agrega los servicios de Asignaturas

---
+++ Services/AsignaturasService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using RegistroEstudiante.DAL;
+using RegistroEstudiante.Models;
+using System.Linq.Expressions;
+
+namespace RegistroEstudiante.Services;
+
+public class AsignaturasService(IDbContextFactory<Contexto> DbFactory)
+{
+    private async Task<bool> Existe(int asignaturaId, String? nombre = null)
+    {
+        if (nombre == null)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Asignaturas.AnyAsync(e => e.AsignaturaId == asignaturaId);
+        }
+        else
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Asignaturas.AnyAsync(e => e.Nombre.ToLower().Trim() == nombre.ToLower().Trim() && e.AsignaturaId != asignaturaId);
+        }
+    }
+    private async Task<bool> Insertar(Asignaturas asignatura)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Asignaturas.Add(asignatura);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+    private async Task<bool> Modificar(Asignaturas asignatura)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Asignaturas.Update(asignatura);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+    public async Task<Asignaturas?> Buscar(int asignaturaId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Asignaturas.FirstOrDefaultAsync(e => e.AsignaturaId == asignaturaId);
+    }
+    public async Task<bool> Eliminar(int asignaturaId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Asignaturas.AsNoTracking().Where(e => e.AsignaturaId == asignaturaId).ExecuteDeleteAsync() > 0;
+    }
+    public async Task<List<Asignaturas>> Listar()
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Asignaturas.ToListAsync();
+    }
+    public async Task<bool> Guardar(Asignaturas asignatura)
+    {
+        if (!await Existe(asignatura.AsignaturaId))
+        {
+            return await Insertar(asignatura);
+        }
+        else
+        {
+            return await Modificar(asignatura);
+        }
+    }
+}
