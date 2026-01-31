b6f9067295c4ce8528e95103a1e16a6d834be723
Jeremy Sanchez Cabrera <jeremy_sanchez@ucne.edu.do>
2026-01-30 20:51:57 -0400

    Agrega extensors de los Enums para un manejo mas facil

---
+++ Extensors/ColorExtensions.cs
@@ -0,0 +1,42 @@
+﻿using RegistroEstudiante.Enums;
+
+namespace RegistroEstudiante.Extensors;
+public static class ColorExtensions
+{
+    public static string GetCssBgColor(Color color)
+    {
+        switch(color){
+            case Color.Rojo:
+                return "bg-danger";
+            case Color.Azul:
+                return "bg-primary";
+            case Color.Verde:
+                return "bg-success";
+            case Color.Rosa:
+                return "#e83e8c";
+            case Color.Amarillo:
+                return "bg-warning";
+            case Color.Gris:
+                return ".bg-secondary";
+            case Color.Negro:
+                return "bg-dark";
+            case Color.Blanco:
+                return "#bg-light";
+            default:
+                return "#bg-dark";
+
+        }
+    }
+
+    public static string GetCssTextColor(Color color)
+    {
+        if (color.Equals(Color.Blanco))
+        {
+            return "text-dark";
+        }
+        else
+        {
+            return "text-light";
+        }
+    }
+}
---
+++ Extensors/IconExtensions.cs
@@ -0,0 +1,32 @@
+﻿using RegistroEstudiante.Enums;
+
+namespace RegistroEstudiante.Extensors;
+public static class IconExtensions
+{
+    public static string GetCssIcon(Icon icono)
+    {
+        switch (icono)
+        {
+            case Icon.PulgarArriba:
+                return "bi-hand-thumbs-up-fill text-success";
+            case Icon.Logro:
+                return "bi-award-fill text-warning";
+            case Icon.Destacado:
+                return "bi-star-fill text-warning";
+            case Icon.Error:
+                return "bi-x-circle-fill text-danger";
+            case Icon.Advertencia:
+                return "bi-exclamation-triangle-fill text-warning";
+            case Icon.PulgarAbajo:
+                return "bi-hand-thumbs-down-fill text-danger";
+            case Icon.Pregunta:
+                return "bi-question-circle-fill text-warning";
+            case Icon.Participacion:
+                return "bi-chat-dots-fill text-primary";
+            case Icon.Reloj:
+                return "bi-clock-fill text-warning";
+            default:
+                return "";
+        }
+    }
+}
